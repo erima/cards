aa0b04b94fc91f3d62eace36807198eeab13d4dc
erima <erima@me.com>
2018-04-25 21:28:00 -0400

    fixed issue where the first click didn't work

--- cards.js
+++ cards.js
@@ -7,43 +7,45 @@ $(document).ready(function() {
   getSuit = $(".suit-selector").each(function() {
     suits.push(this.id);
   });
+   console.log("suits: " + suits);
 
-  console.log("suits: " + suits);
+
+  var cardNumber = [
+    "a",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "j",
+    "q",
+    "k",
+    "joker",
+    "joker"
+  ];
 
   $("li").click(function() {
     suitRequest = this.id;
-
     console.log("suitRequest: " + suitRequest);
-
-    var cardNumber = [
-      "a",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "j",
-      "q",
-      "k",
-      "joker",
-      "joker"
-    ];
-
     numberRandom = cardNumber[Math.floor(Math.random() * cardNumber.length)];
     console.log("randompick: " + numberRandom);
-    console.log("card-" + suitRequest + "&nbsp;" + "card-" + numberRandom);
 
-    $("li").click(function(){
-      switch(suitRequest) {
-        case "joker":
-          return $("#result").removeClass().addClass("card " + "card-" + suitRequest);
-        default:
-          return $("#result").removeClass().addClass("card " + "card-" + suitRequest + " " + "card-" + numberRandom);
-    };
-    });
+    console.log("card-" + suitRequest + "&nbsp;" + "card-" + numberRandom);
+    switch (suitRequest) {
+      case "joker":
+        return $("#result")
+          .removeClass()
+          .addClass("card " + "card-" + suitRequest);
+      default:
+        return $("#result")
+          .removeClass()
+          .addClass(
+            "card " + "card-" + suitRequest + " " + "card-" + numberRandom
+          );
+    }
   });
 });
